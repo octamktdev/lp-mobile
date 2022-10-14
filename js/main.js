0a89ed04b3a452b1dde1b7ff85df0e21ac62c88e
octamktdev <marketing@octadesk.com>
2022-10-14 17:36:25 -0300

    adicionando pagina do esquenta e sistema de whatsapp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('body').on('focus', 'phone-df03f2ff-834d-41bd-8187-cd8b844db1fa', function(){
+    $('body').on('focus', 'telefone', function(){
         var maskBehavior = function (val) {
             return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
         },
